e0ef602e99d07fe935e948178e6d6b600dd749b4
Jacha <54313904+sspaang@users.noreply.github.com>
2020-07-19 03:44:48 -0700

    Fourth attemp
    Add menu

--- Assignment1.sh
+++ Assignment1.sh
@@ -1,38 +1,69 @@
 #!/bin/bash
 
 #GROUP NO. start:19-07-2020 finish:
-echo "          DATE"
-date +"  %A %d %B %Y"
-BEyear=2563
-date +"  %A %d %B $BEyear"
-echo "_________________________"
-
-#RANDOM NUMBER GAME 1-10
-echo ""
-echo "=========== NUMBER RANDOM GAME ==========="
-echo ""
-random=$[($RANDOM % 10 + 1)]
-#User input
-
+#========MENU===========
 while true
 do
-	read -p "Guess my number (1-10): " gnum
-	if [ $gnum -eq $random ]
+	echo "---------------------------MENU----------------------------"
+	echo ""
+	echo "1. Show date"
+	echo "2. Files managment"
+	echo "3. Play 'Number random game'"
+	read -p "Enter number to choose what do you want to do: " cnum
+	clear
+
+	if [ $cnum -eq 1 ]
 	then
-		echo "Correct! Why are you so brilliant :D"
+		echo "          DATE"
+		date +"  %A %d %B %Y"
+		BEyear=2563
+		date +"  %A %d %B $BEyear"
+		echo "_________________________"
+	elif [ $cnum -eq 2 ]
+	then
+		continue
+	elif [ $cnum -eq 3 ]
+	then 
+		#RANDOM NUMBER GAME 1-10
 		echo ""
-		echo "=========================================="
-		break
-	else
-		if [ $gnum -lt $random ]
-		then
-			echo "The number is greater than that :O"
-			echo "HINT: $((gnum+1))-10"
-		elif [ $gnum -gt $random ]
-		then
-			echo "The number is lesser than that :D"
-			echo "HINT: 1-$((gnum-1))"
-		fi
+		echo "=========== NUMBER RANDOM GAME ==========="
+		echo ""
+		random=$[($RANDOM % 10 + 1)]
+		#User input
+
+		while true
+		do
+			read -p "Guess my number (1-10): " gnum
+			if [ $gnum -eq $random ]
+			then
+				echo "Correct! Why are you so brilliant :D"
+				echo ""
+				echo "=========================================="
+				break
+			else
+				if [ $gnum -lt $random ]
+				then
+					echo "The number is greater than that :O"
+					echo "HINT: $((gnum+1))-10"
+				elif [ $gnum -gt $random ]
+				then
+					echo "The number is lesser than that :D"
+					echo "HINT: 1-$((gnum-1))"
+				fi
+			fi
+		done
 	fi
+	
+	read -p "Do you want to continue?[y/n]" ans
+	
+	if [ "$ans" = "y" ]
+	then 
+		clear
+		continue
+	elif [ "$ans" = "n" ]
+	then
+		break
+	fi
+	
 done
 
