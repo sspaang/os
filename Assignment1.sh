212a246ce6c98d08ad47d119bfd3cf3cb9ce90c9
Jacha <54313904+sspaang@users.noreply.github.com>
2020-07-19 02:25:30 -0700

    First commit
    date + number random game

---
+++ Assignment1.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#GROUP NO. start:19-07-2020 finish:
+echo "          DATE"
+date +"  %A %d %B %Y"
+BEyear=2563
+date +"  %A %d %B $BEyear"
+echo "_________________________"
+
+#RANDOM NUMBER GAME 1-10
+echo ""
+echo "=========== NUMBER RANDOM GAME ==========="
+echo ""
+random=$[($RANDOM % 10 + 1)]
+#User input
+
+while true
+do
+	read -p "Guess my number (1-10): " gnum
+	if [ $gnum -eq $random ]
+	then
+		echo "Correct! Why are you so brilliant :D"
+		echo ""
+		echo "=========================================="
+		break
+	else
+		if [ $gnum -le $random ]
+		then
+			echo "The number is greater than that :O"
+			echo "HINT: >= $gnum"
+		elif [ $gnum -ge $random ]
+		then
+			echo "The number is lesser than that :D"
+			echo "HINT: <= $gnum"
+		fi
+	fi
+done
+
